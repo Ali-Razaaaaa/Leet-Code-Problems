7cb2a284dc0daf9b0d94ced70ddc070556610f59
alirazabugti1 <razabugtiali@gmail.com>
2025-03-20 23:00:16 +0500

    Ispalindrome

---
+++ 2 Pointers/isPalindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseString(const string &s) {
+        string result(s.length(), ' '); // Pre-allocate memory
+        int j = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result[j++] = s[i];
+        }
+        return result;
+    }
+
+    bool isPalindrome(string s) {
+        string filtered = "";
+        for (int i = 0; i < s.length(); i++) { // Traditional for-loop
+            if (isalnum(s[i])) filtered += tolower(s[i]);
+        }
+        return filtered == reverseString(filtered);
+    }
+};
