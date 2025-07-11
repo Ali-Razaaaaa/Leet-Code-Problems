7f5a399ea7b0dc302b1c1b1a510979e50c2f1d81
alirazabugti1 <razabugtiali@gmail.com>
2025-07-12 01:22:45 +0500

    solved

--- Stack/Minimum Stack.cpp
+++ Stack/Minimum Stack.cpp
@@ -1,33 +1,37 @@
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class MinStack {
 public:
-    vector<int> stack;  
+    vector<int> stack;
+    vector<int> minStack;
 
-    MinStack() {
+    MinStack() {}
 
-    }
-    
     void push(int val) {
         stack.push_back(val);
+
+        // Push to minStack if it's the first element or a new minimum
+        if (minStack.empty() || val <= minStack.back()) {
+            minStack.push_back(val);
+        }
     }
-    
+
     void pop() {
         if (!stack.empty()) {
+            // Also pop from minStack if the value being removed is the min
+            if (stack.back() == minStack.back()) {
+                minStack.pop_back();
+            }
             stack.pop_back();
         }
     }
-    
+
     int top() {
         return stack.back();
     }
-    
+
     int getMin() {
-        if (!stack.empty()) {
-            return *min_element(stack.begin(), stack.end());
-        }
-        return -1;
+        return minStack.back(); // O(1) access to current min
     }
 };
